32ed3aacc1a909b77e40c992c5b1d0df4c144e31
leonard <1040926894@qq.com>
2018-03-04 22:45:53 +0800

    UI Widget 3-4-home

--- app/src/main/java/com/wangqian/uiwidgettest/MainActivity.java
+++ app/src/main/java/com/wangqian/uiwidgettest/MainActivity.java
@@ -1,13 +1,82 @@
 package com.wangqian.uiwidgettest;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private EditText editText;
+    private ImageView imageView;
+    private ProgressBar progressBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button=findViewById(R.id.button);
+        button.setOnClickListener(this);
+        editText= findViewById(R.id.edit_text);
+        imageView=findViewById(R.id.image_view);
+        progressBar=findViewById(R.id.progress_bar);
+//        button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//
+//            }
+//        });
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.button:
+                //获取输入的值
+//                String inputTest=editText.getText().toString();
+//                Toast.makeText(MainActivity.this,inputTest,Toast.LENGTH_LONG).show();
+                //切换图片
+//                imageView.setImageResource(R.drawable.img_2);
+                //更改可见状态
+//                if(progressBar.getVisibility()==View.GONE){
+//                    progressBar.setVisibility(View.VISIBLE);
+//                }else {
+//                    progressBar.setVisibility(View.GONE);
+//                }
+                //加进度条
+//                int progress=progressBar.getProgress();
+//                progress=progress+10;
+//                progressBar.setProgress(progress);
+                //对话框
+//                AlertDialog.Builder dialog=new AlertDialog.Builder(MainActivity.this);
+//                dialog.setTitle("This is Dialog");
+//                dialog.setMessage("FBI WARN! EAST HOT");
+//                dialog.setCancelable(false);
+//                dialog.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+//                    @Override
+//                    public void onClick(DialogInterface dialogInterface, int i) {
+//
+//                    }
+//                });
+//                dialog.setNegativeButton("Cancle", new DialogInterface.OnClickListener() {
+//                    @Override
+//                    public void onClick(DialogInterface dialogInterface, int i) {
+//
+//                    }
+//                });
+//                dialog.show();
+
+                //进度条对话框
+                ProgressDialog progressDialog=new ProgressDialog(MainActivity.this);
+                progressDialog.setTitle("This is ProgressDialog");
+                progressDialog.setMessage("Loading...");
+                progressDialog.setCancelable(false);
+                progressDialog.show();
+                break;
+                default:
+                    break;
+        }
     }
 }
